b3844537f61ee648fd9f70190f023f4d5a53a6f1
zoop-potato <joshbutler2084@gmail.com>
2023-10-16 14:54:05 -0400

    Test print pub and priv keys in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::*;
+use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let bits = 2048;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let pub_key = RsaPublicKey::from(&priv_key);
+
+    println!("{:?}", priv_key);
+    println!();
+    println!("{:?}", pub_key);
 }
